f520756a195035005fb4f3273b3a5e624971d606
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2020-06-15 20:28:08 -0400

    Layout completion before adding API data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <b-nav>
-    <b-nav-item disabled>Fancy Quiz App</b-nav-item>
+    <b-nav tabs>
+    <b-nav-item disabled><strong>Fancy Quiz App</strong></b-nav-item>
     <b-nav-item disabled>Counter: 4/10</b-nav-item>
   </b-nav>
   </div>
